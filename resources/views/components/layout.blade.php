da2bdd9ec603e7939c9dd4e8cc5a76fee513156c
Dame <gban1mj27@gmail.com>
2025-06-02 18:18:43 +0700

    removed blade inheritance template, changed to components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,11 @@
+<!-- When there is no desire, all things are at peace. - Laozi -->
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title') Page</title>
+  <title>{{ $title }}</title>
   @vite('resources/css/app.css')
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
@@ -12,15 +13,12 @@
 
 <body class="h-full">
   <div class="min-h-full">
-    @include('partials.navbar')
-    <header class="bg-white shadow-sm">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">@yield('title')</h1>
-      </div>
-    </header>
+    <x-navbar />
+
+    <x-header :title="$title" />
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        @yield('content')
+        {{ $slot }}
       </div>
     </main>
   </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,5 @@
+<!-- Well begun is half done. - Aristotle -->
+
 <nav class="bg-gray-800" x-data="{ isOpen: false }">
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="flex h-16 items-center justify-between">
@@ -110,6 +112,16 @@
               d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
           </svg>
         </button>
+      </div>
+      <div class="mt-3 space-y-1 px-2">
+        <a href="#"
+          class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+          Profile</a>
+        <a href="#"
+          class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
+        <a href="#"
+          class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
+          out</a>
       </div>
     </div>
   </div>
