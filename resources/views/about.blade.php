9a68ad58557065efdaeb36bb4354b2e4f20fb432
Dame <gban1mj27@gmail.com>
2025-06-02 12:24:29 +0700

    added some pages and js

---
+++ resources/views/about.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Welcome to About Page</h1>
+    <ul>
+        <li>
+            <a href="/home">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/contact">Contact</a>
+        </li>
+    </ul>
+</body>
+<script src="js/script.js"></script>
+
+</html>
